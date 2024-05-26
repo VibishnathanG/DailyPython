c8cf0e4ddc04e44f1e76a64eb90a18079f5090d5
Vibish <Vibish@gmail.com>
2024-05-27 00:04:15 +0530

    27-05 -> Progress........

---
+++ w3-sample1.py
@@ -0,0 +1,27 @@
+# 1
+
+# print ("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\n\sTwinkl0e, twinkle, little star,\n\tHow I wonder what you are")
+
+# 2
+
+# import sys
+# print(sys.version)
+
+# 3
+
+# import datetime as dt
+
+# print("Current Data and Time : \n" + dt.datetime.now().strftime("%Y-%m-%d  %H:%M:%S"))
+
+#4
+# import math
+# r = 1.1
+# print(math.pi*pow(r, 2))
+
+#5
+
+# fname= input("Enter Fname: ")
+# lname= input("Enter lname: ")
+
+# print(lname+ " " + fname)
+
