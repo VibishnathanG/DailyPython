c3f8fc68af15a5b5facc57f9864db0d1854c4830
Vibish <Vibish@gmail.com>
2024-05-27 22:29:53 +0530

    27-05-2024 --> Progress

--- w3-sample1.py
+++ w3-sample1.py
@@ -25,3 +25,30 @@
 
 # print(lname+ " " + fname)
 
+#6
+
+# a= input("Enter Comma Separated Word: ").split(",")
+
+# print(a)
+# print(tuple(a))
+
+#7
+
+# a= input("Enter File Name: ")
+# print(a.split(".")[-1])
+
+#8
+
+# a=input("Enter Space Separeted Colour: ").split(" ")
+# print("First Element: "+a[0]+"\n"+"Last Element: " + a[-1])
+
+#9
+
+# a=(11, 12, 2014)
+# print("Exam Date is : %i / %i / %i " % a )
+
+#10
+
+# a=int(input("Enter n: "))
+# b= ("%i + %i%i + %i%i%i" % (a,a,a,a,a,a)).split("+")
+# print(sum(int(ele) for ele in b))
