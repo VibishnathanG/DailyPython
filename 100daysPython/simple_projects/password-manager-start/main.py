df6185a66c84e950534d7d68820183f79ce1aeab
Vibish-WSL <vibishdevops@gmail.com>
2025-10-28 14:16:53 +0000

    password maanger

---
+++ 100daysPython/simple_projects/password-manager-start/main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+window = tk.Tk()
+window.title("Password Manager")
+window.geometry("500x500")
+window.config(padx=50, pady=50)
+window.ima
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+
+window.mainloop()
